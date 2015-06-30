c2dfbeee1751d033e573f12416905c4de46645e6
travisneilson <travisneilson@gmail.com>
2015-06-30 16:28:50 -0700

    Maggies suggestions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-$(window).scroll(function(){
+$(window).scroll(function() {
 
   var wScroll = $(window).scrollTop();
 
@@ -9,69 +9,82 @@ $(window).scroll(function(){
 });
 
 
-$(function(){
+var windowWidth = {
+  large: $(window).width() > 640
+};
+
+$(function() {
   mentoringBubble();
 });
 
 
 function mentoringBubble() {
 
-  $('.face').on('click', function(){
-    var faceTop = $(this).position().top,
-        vertMath = -1 * (faceTop - 240),
-        faceLeft = $(this).position().left,
-        leftMath = 0 - faceLeft;
-
-    if ($(window).width() > 640){
-      $('.face-wrap').css('top', vertMath + 'px');
-    } else {
-      if($(this).hasClass('back-btn')){
-        mentoringNarrowStart();
-      } else{
-        $('.face-wrap').css('left', leftMath + 'px');
-        $('.bubble').css('left', faceLeft + 'px');
-      }
-    }
-    if(!$(this).hasClass('back-btn')){
-      $(this).addClass('has-open-bubble')
-      .siblings().removeClass('has-open-bubble');
-    }
-  });
-
-
   // click something
   // get its distance from the top of its parent
   // move everything to that the perfect distance (240) - items distance
   // add class of is-open on it, so it pop's the balloon
 
+  $('.face').on('click', function() {
+    var faceTop = $(this).position().top,
+        vertMath = -1 * (faceTop - 240),
+        faceLeft = $(this).position().left,
+        leftMath = 0 - faceLeft;
+
+    if (windowWidth.large) {
+      $('.face-wrap').css('top', vertMath + 'px');
+
+    } else {
+
+      if($(this).hasClass('back-btn')){
+        mentoringNarrowStart();
+
+      } else {
+        $('.face-wrap').css('left', leftMath + 'px');
+        $('.bubble').css('left', faceLeft + 'px');
+      }
+
+    }
+
+    if (!$(this).hasClass('back-btn')) {
+      $(this).addClass('has-open-bubble')
+        .siblings().removeClass('has-open-bubble');
+    }
+
+  });
+
 }
 function mentoringWideStart() {
   $('.face:nth-child(3)').addClass('has-open-bubble')
-  .siblings().removeClass('has-open-bubble');
+    .siblings().removeClass('has-open-bubble');
+
   $('.face-wrap').css({
     'left': '0px',
     'top': '0px'
   });
+
   $('.bubble').css('left','100%');
 }
 
-function mentoringNarrowStart(){
+function mentoringNarrowStart() {
   $('.face').first().addClass('has-open-bubble')
-  .siblings().removeClass('has-open-bubble');
+    .siblings().removeClass('has-open-bubble');
+
   $('.face-wrap').css({
     'left': '0px',
     'top': '0px'
   });
+
   $('.bubble').css('left','0px');
 }
 
-function startMentoring(wScroll){
-  if($('section.mentoring').offset().top - 200 < wScroll){
+function startMentoring(wScroll) {
+  if ($('section.mentoring').offset().top - 200 < wScroll) {
     $('.face-wrap').addClass('launched');
 
-    setTimeout(function(){
-      if(!$('.face').hasClass('has-open-bubble')) {
-        if($(window).width() > 640){
+    setTimeout(function() {
+      if (!$('.face').hasClass('has-open-bubble')) {
+        if (windowWidth.large) {
           mentoringWideStart();
         } else {
           mentoringNarrowStart();
@@ -81,8 +94,9 @@ function startMentoring(wScroll){
   }
 }
 
-$(window).resize(function(){
-  if($(window).width() > 640){
+$(window).resize(function() {
+  windowWidth.large = $(window).width() > 640;
+  if (windowWidth.large) {
     mentoringWideStart();
   } else {
     mentoringNarrowStart();
