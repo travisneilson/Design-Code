cf39db03754ad9aba223bf7d410e674c62a01fbd
travisneilson <travisneilson@gmail.com>
2015-08-16 06:07:06 -0700

    Completed Recording1

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,20 @@
 $(function() {
   mentoringBubbleClick();
   setInterval(function(){articleTada()}, 4000);
+  designBGStuff();
 
 });
+
+function designBGStuff() {
+  $('.design-img-link').hover(function(){
+    $(this).parent().parent().css('background-color', $(this).data('color'));
+  }, function(){
+    // off > revert the color
+    $(this).parent().parent().css('background-color', $(this).parent().parent().data('orig-color'));
+  });
+
+
+}
 
 function articleTada(){
   var randNum = Math.floor(Math.random() * $('.article-thumb').length) +1
